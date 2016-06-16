ee651a8a143018c56cabb0900b13cd6c55696dc7
YUMI NARITA <narita.yumi@canon-its.co.jp>
2016-06-16 13:00:37 +0000

    portion　template => render form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'messages#index'
-  resources :messages
+  resources :messages , except: [:index, :new]
+  
   # resources :messages , only: [:create]
 
   # The priority is based upon order of creation: first created -> highest priority.
